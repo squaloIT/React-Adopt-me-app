2900f90bdf819820ba8627a286961a7850424c78
Nikola Mihajlovic <nikola.mihajlovic@ict.edu.rs>
2019-08-18 21:13:32 +0200

    JSX dodat u projekat umesto createElement

--- app/src/App.js
+++ app/src/App.js
@@ -3,34 +3,40 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {}, // Atributi taga
-    [
-      React.createElement("h1", {}, "Adopt me!"), // Sadrzaj - content
-      // React.createElement("h1", {} , "Adopt me!"), // Jos jedan content koji ce biti pored
-      React.createElement(Pet, {
-        name: "Ceda",
-        animal: "dog",
-        breed: "ZVERINA"
-      }),
-      React.createElement(Pet, {
-        name: "Mica",
-        animal: "dog",
-        breed: "Alkoholicar pas"
-      }),
-      React.createElement(Pet, {
-        name: "Pera",
-        animal: "dog",
-        breed: "Pas limar"
-      }),
-      React.createElement(Pet, {
-        name: "Sneza",
-        animal: "dog",
-        breed: "Pekarka pas"
-      })
-      // Posto je ovo moja komponenta, ovi atributi se prosledjuju do komponente kao param. Za razliku od taga kada budu njegovi param
-    ]
+  // return React.createElement(
+  //   "div",
+  //   {}, // Atributi taga
+  //   [
+  //     React.createElement("h1", {}, "Adopt me!"), // Sadrzaj - content
+  //     // React.createElement("h1", {} , "Adopt me!"), // Jos jedan content koji ce biti pored
+  //     React.createElement(Pet, {
+  //       name: "Ceda",
+  //       animal: "dog",
+  //       breed: "ZVERINA"
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Mica",
+  //       animal: "dog",
+  //       breed: "Alkoholicar pas"
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Pera",
+  //       animal: "dog",
+  //       breed: "Pas limar"
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Sneza",
+  //       animal: "dog",
+  //       breed: "Pekarka pas"
+  //     })
+  //     // Posto je ovo moja komponenta, ovi atributi se prosledjuju do komponente kao param. Za razliku od taga kada budu njegovi param
+  //   ]
+  // );
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Ceda" breed="ZIVULJKA" />
+    </div>
   );
 };
 
--- app/src/Pet.js
+++ app/src/Pet.js
@@ -3,9 +3,15 @@ import React from "react";
 // const Pet = props => {
 export default function Pet({ name, breed }) {
   // KORISTIM DESCTRUCTURING UMESTO CELOG PROPS
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name + " " + breed),
-    React.createElement("h2", {}, name + " " + breed),
-    React.createElement("h3", {}, name + " " + breed)
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, name + " " + breed),
+  //   React.createElement("h2", {}, name + " " + breed),
+  //   React.createElement("h3", {}, name + " " + breed)
+  // ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
