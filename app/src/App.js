27dd531cd0a56dacda8a5624376a79decdc019fc
Nikola Mihajlovic <nikola.mihajlovic@ict.edu.rs>
2019-08-18 18:15:06 +0200

    Dodajem prettierrc fajl

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,12 @@
 // const Pet = props => {
-const Pet = ({ animal, name, breed }) => { // KORISTIM DESCTRUCTURING UMESTO CELOG PROPS
+const Pet = ({ animal, name, breed }) => {
+  // KORISTIM DESCTRUCTURING UMESTO CELOG PROPS
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name + " " + breed),
     React.createElement("h2", {}, name + " " + breed),
     React.createElement("h3", {}, name + " " + breed)
   ]);
-}
-
+};
 
 const App = () => {
   return React.createElement(
@@ -15,16 +15,29 @@ const App = () => {
     [
       React.createElement("h1", {}, "Adopt me!"), // Sadrzaj - content
       // React.createElement("h1", {} , "Adopt me!"), // Jos jedan content koji ce biti pored
-      React.createElement(Pet, { name: "Ceda", "animal": "dog", "breed": "ZVERINA" }),
-      React.createElement(Pet, { name: "Mica", "animal": "dog", "breed": "Alkoholicar pas" }),
-      React.createElement(Pet, { name: "Pera", "animal": "dog", "breed": "Pas limar" }),
-      React.createElement(Pet, { name: "Sneza", "animal": "dog", "breed": "Pekarka pas" })
+      React.createElement(Pet, {
+        name: "Ceda",
+        animal: "dog",
+        breed: "ZVERINA"
+      }),
+      React.createElement(Pet, {
+        name: "Mica",
+        animal: "dog",
+        breed: "Alkoholicar pas"
+      }),
+      React.createElement(Pet, {
+        name: "Pera",
+        animal: "dog",
+        breed: "Pas limar"
+      }),
+      React.createElement(Pet, {
+        name: "Sneza",
+        animal: "dog",
+        breed: "Pekarka pas"
+      })
       // Posto je ovo moja komponenta, ovi atributi se prosledjuju do komponente kao param. Za razliku od taga kada budu njegovi param
     ]
   );
-}
+};
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
