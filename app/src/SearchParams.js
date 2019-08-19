bf7cf6f1d762542f0b9d87009cb14d12e9188192
Nikola Mihajlovic <nikola.mihajlovic@ict.edu.rs>
2019-08-19 15:54:37 +0200

    Dodat useEffect kada se promeni ddl Animal da se promeni i rasa
    Dodat ScrictMode aplikaciji

--- app/src/SearchParams.js
+++ app/src/SearchParams.js
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useDropdown from "./useDropdown";
-import { ANIMALS } from "@frontendmasters/pet";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 //Kada postavim @ ispred nekog import-a, a imam instaliran parcer, on ce ovo sto sam importovao instalirati za mene kroz npm!
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breeds", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
   //location je trenutna vrednost, a set location je setter funkcija. useState kada se pozove vraca niz sa dva elementa, prvi koji drzi trenutno stanje a drugi func koja menja to stanje.
   // Ovo je HOOK, svi Hook-ovi pocinju sa use.
   // Hooks nikada ne idu unutar if-a ili for-a ili bilo kod statementa.
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+    //Moram da postavim default stanje kada god se zivuljka za koju dohvatam rase promeni. Jer u suprotnom ostaje jedna zivotinja (pas) a rasa macke.
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedNames = breeds.map(({ name }) => name);
+      setBreeds(breedNames);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
+  //Drugi parametar useEffect-a definise nakon promene cega ce se trigerovati funkcija useEffect-a. To je njen takozvani dependency
+  // setBreed i setBreeds su ovde dodati samo zbog ESLINT, inace promena useEffect zavisi samo i jedino od animal
+  //Ako zelim da se useEffect pozove samo jedan jedini put, kada se prvi put mountuje. OSTAVIM DRUGI PARAMETAR DA BUDE PRAZAN NIZ
+  // Ako zelim svaki put da se update-uje, onda izbrisem drugi parametar.
 
   return (
     <div className="search-params">
